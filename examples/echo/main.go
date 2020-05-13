a7d699eac8a3e3afcf7766692819e620650cf4e7
hannes <hannesdewaal@gmail.com>
2020-05-13 14:34:16 +0200

    Updat emodule name

---
+++ examples/echo/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	"github.com/labstack/echo"
+
+	//	_ "github.com/mattn/go-sqlite3"
+	//_ "github.com/jinzhu/gorm/dialects/mysql"
+	// _ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	// _ "github.com/jinzhu/gorm/dialects/mssql"
+
+	"github.com/windhooked/session/manager"
+	"github.com/windhooked/thor/admin"
+	"github.com/windhooked/thor/auth"
+	"github.com/windhooked/thor/auth_themes/clean"
+	"github.com/windhooked/thor/qor"
+	"github.com/windhooked/thor/qor/auth/auth_identity"
+)
+
+type User struct {
+	gorm.Model
+	Email    string
+	Password string
+	Name     string
+	Gender   string
+	Role     string
+}
+
+type AdminAuth struct{}
+
+var (
+	DB, _ = gorm.Open("sqlite3", "example.db")
+
+	Auth = clean.New(&auth.Config{
+		DB:                DB,
+		UserModel:         &User{},
+		AuthIdentityModel: &auth_identity.AuthIdentity{},
+	})
+)
+
+func (user *User) DisplayName() string {
+	return user.Name
+}
+
+func (AdminAuth) LoginURL(c *admin.Context) string {
+	return "/auth/login"
+}
+
+func (AdminAuth) LogoutURL(c *admin.Context) string {
+	return "/auth/logout"
+}
+
+func (AdminAuth) GetCurrentUser(c *admin.Context) qor.CurrentUser {
+	currentUser := Auth.GetCurrentUser(c.Request)
+	if currentUser != nil {
+		qorCurrentUser, ok := currentUser.(qor.CurrentUser)
+		if !ok {
+			fmt.Printf("User %#v haven't implement qor.CurrentUser interface\n", currentUser)
+		}
+		return qorCurrentUser
+	}
+	return nil
+}
+
+func main() {
+	// Setup DB
+	DB.AutoMigrate(&User{})
+	DB.LogMode(true)
+
+	// Setup Admin
+	Admin := admin.New(&admin.AdminConfig{
+		DB:   DB,
+		Auth: &AdminAuth{},
+	})
+	Admin.AddResource(&User{})
+
+	// Setup Echo
+	e := echo.New()
+
+	// Middleware
+	authMiddleware := echo.WrapMiddleware(manager.SessionManager.Middleware)
+	e.Use(authMiddleware)
+
+	// Handler
+	adminHandler := echo.WrapHandler(Admin.NewServeMux("/admin"))
+	e.GET("/admin", adminHandler)
+	e.Any("/admin/*", adminHandler)
+
+	authHandler := echo.WrapHandler(Auth.NewServeMux())
+	e.Any("/auth/*", authHandler)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
